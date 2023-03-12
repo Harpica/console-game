828791c4f491a9a2364b5047181a120e60f52143
Harpica <harpy.ka@gmail.com>
2023-03-12 16:11:20 +0300

    feat: inputMaintainer and main game() function

--- game.ts
+++ game.ts
@@ -1,4 +1,5 @@
 import crypto, { Hmac } from "crypto";
+import readline from "readline";
 
 type Result = "win" | "lose" | "draw";
 
@@ -8,7 +9,7 @@ class Rules {
   constructor(values: Array<string>) {
     this.values = values;
     this.halfNumberOfTurns = Math.floor(values.length / 2);
-    console.log(this.halfNumberOfTurns);
+    this.checkWinner = this.checkWinner.bind(this);
   }
   public checkWinner(firstValue: number, secondValue: number): Result {
     const lastWin = (firstValue + this.halfNumberOfTurns) % this.values.length;
@@ -33,7 +34,7 @@ class HelpTable {
   private table: Array<Array<string>>;
   constructor(rules: Rules) {
     this.rules = rules;
-    this.table = [rules.values];
+    this.table = [[...this.rules.values]];
     this.createRulesTable();
   }
   private createRulesTable() {
@@ -71,13 +72,96 @@ class HmacGenerator {
   }
 }
 
-const values = process.argv.slice(2);
-const rules = new Rules(values);
-const helpTable = new HelpTable(rules);
-const key = new KeyGenerator();
-const hmac = new HmacGenerator();
+function getOptions(values: Array<string>): string {
+  let optionsString = "Available moves:\n";
+  values.forEach((value, i) => {
+    optionsString += `${i} - ${value}\n`;
+  });
+  optionsString += "q - exit\n? - help\n";
+  optionsString += "Enter your move: ";
+  return optionsString;
+}
 
-// helpTable.showTable();
-key.createKey();
-hmac.createHmac(key.key, "Hello");
-console.log(hmac.hmac, key.key);
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+const ac = new AbortController();
+const signal = ac.signal;
+
+function proceedInputOptions(
+  input: string,
+  values: Array<string>,
+  computerIndex: number,
+  optionsMaintainer: {
+    checkWinner: (a: number, b: number) => Result;
+    help: Function;
+    key: string;
+  }
+) {
+  const { checkWinner, help, key } = optionsMaintainer;
+  const inputIndex = parseInt(input);
+  if (input === "?") {
+    help();
+  } else if (input === "q") {
+    ac.abort();
+    process.exit();
+  } else if (!isNaN(inputIndex)) {
+    if (inputIndex >= 0 && inputIndex < values.length) {
+      console.log("Your move: " + values[inputIndex]);
+      console.log("Computer's move: " + values[computerIndex]);
+      switch (checkWinner(inputIndex, computerIndex)) {
+        case "win":
+          console.log("You win!");
+          break;
+        case "lose":
+          console.log("You lose :C");
+          break;
+        case "draw":
+          console.log("Dead heat!");
+          break;
+        default:
+          break;
+      }
+      console.log("Secret key: " + key);
+      process.exit();
+    }
+  } else {
+    console.log("Please enter valid value from the table above");
+  }
+  ac.abort();
+  rl.question("Your move: ", (answer) =>
+    proceedInputOptions(answer, values, computerIndex, {
+      checkWinner,
+      help,
+      key,
+    })
+  );
+}
+
+function getIndexOfComputerValue(values: Array<string>): number {
+  return Math.floor(Math.random() * values.length);
+}
+
+function game() {
+  const values = process.argv.slice(2);
+  const rules = new Rules(values);
+  const helpTable = new HelpTable(rules);
+  const key = new KeyGenerator();
+  const hmac = new HmacGenerator();
+  key.createKey();
+  const computerIndex = getIndexOfComputerValue(values);
+  hmac.createHmac(key.key, values[computerIndex]);
+  console.log("HMAC: ", hmac.hmac);
+  rl.question(getOptions(values), { signal }, (answer) =>
+    proceedInputOptions(answer, values, computerIndex, {
+      checkWinner: rules.checkWinner,
+      help: () => {
+        helpTable.showTable();
+      },
+      key: key.key,
+    })
+  );
+}
+
+game();
