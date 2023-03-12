65b7d165626a3a37504e9792207aff0f1fa40d21
Harpica <harpy.ka@gmail.com>
2023-03-12 17:27:18 +0300

    fix: arguments check

--- game.ts
+++ game.ts
@@ -8,9 +8,13 @@ import { getIndexOfComputerValue } from './modules/utils';
 function game() {
   // Getting array of values from env variables
   const values = process.argv.slice(2);
-  // If values[] is empty or the number of them is even - exiting program
-  if (!values.length || values.length % 2 === 0) {
-    console.log('Please enter odd number of turns as arguments');
+  // Getting only uniq values for check of the number of values
+  const valuesSet = new Set(values);
+  // If valuesSet is empty or the number of them is even - exiting program
+  if (valuesSet.size < 1 || valuesSet.size % 2 === 0) {
+    console.log(
+      'Please enter odd number of turns as arguments. \nThe number of arguments should be equal or more than 3. Values should be uniq.'
+    );
     process.exit();
   }
   // Creating controllers
