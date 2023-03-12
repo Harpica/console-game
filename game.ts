54bc9ee221ce592238d976f484b6f158d92155d4
Harpica <harpy.ka@gmail.com>
2023-03-12 16:38:43 +0300

    style: eslint and prettier + feat: number of args check

--- game.ts
+++ game.ts
@@ -1,7 +1,8 @@
-import crypto, { Hmac } from "crypto";
-import readline from "readline";
+import crypto from 'crypto';
+import readline from 'readline';
+import { table } from 'table';
 
-type Result = "win" | "lose" | "draw";
+type Result = 'win' | 'lose' | 'draw';
 
 class Rules {
   public values: Array<string>;
@@ -17,15 +18,15 @@ class Rules {
 
     if (isOver) {
       if (secondValue > firstValue || secondValue <= lastWin) {
-        return "win";
+        return 'win';
       }
     } else if (secondValue > firstValue && secondValue <= lastWin) {
-      return "win";
+      return 'win';
     }
     if (secondValue === firstValue) {
-      return "draw";
+      return 'draw';
     }
-    return "lose";
+    return 'lose';
   }
 }
 
@@ -45,40 +46,40 @@ class HelpTable {
       }
       this.table.push(turnChecks);
     });
-    this.table[0].unshift("");
+    this.table[0].unshift('');
   }
   showTable() {
-    console.table(this.table);
+    console.log(table(this.table));
   }
 }
 
 class KeyGenerator {
   public key: string;
   constructor() {
-    this.key = "";
+    this.key = '';
   }
   createKey() {
-    this.key = crypto.randomBytes(32).toString("hex");
+    this.key = crypto.randomBytes(32).toString('hex');
   }
 }
 
 class HmacGenerator {
   public hmac: string;
   constructor() {
-    this.hmac = "";
+    this.hmac = '';
   }
   createHmac(key: string, value: string) {
-    this.hmac = crypto.createHmac("sha3-256", key).update(value).digest("hex");
+    this.hmac = crypto.createHmac('sha3-256', key).update(value).digest('hex');
   }
 }
 
 function getOptions(values: Array<string>): string {
-  let optionsString = "Available moves:\n";
+  let optionsString = 'Available moves:\n';
   values.forEach((value, i) => {
     optionsString += `${i} - ${value}\n`;
   });
-  optionsString += "q - exit\n? - help\n";
-  optionsString += "Enter your move: ";
+  optionsString += 'q - exit\n? - help\n';
+  optionsString += 'Enter your move: ';
   return optionsString;
 }
 
@@ -95,42 +96,42 @@ function proceedInputOptions(
   computerIndex: number,
   optionsMaintainer: {
     checkWinner: (a: number, b: number) => Result;
-    help: Function;
+    help: () => void;
     key: string;
   }
 ) {
   const { checkWinner, help, key } = optionsMaintainer;
   const inputIndex = parseInt(input);
-  if (input === "?") {
+  if (input === '?') {
     help();
-  } else if (input === "q") {
+  } else if (input === 'q') {
     ac.abort();
     process.exit();
   } else if (!isNaN(inputIndex)) {
     if (inputIndex >= 0 && inputIndex < values.length) {
-      console.log("Your move: " + values[inputIndex]);
+      console.log('Your move: ' + values[inputIndex]);
       console.log("Computer's move: " + values[computerIndex]);
       switch (checkWinner(inputIndex, computerIndex)) {
-        case "win":
-          console.log("You win!");
+        case 'win':
+          console.log('You win!');
           break;
-        case "lose":
-          console.log("You lose :C");
+        case 'lose':
+          console.log('You lose :C');
           break;
-        case "draw":
-          console.log("Dead heat!");
+        case 'draw':
+          console.log('Dead heat!');
           break;
         default:
           break;
       }
-      console.log("Secret key: " + key);
+      console.log('Secret key: ' + key);
       process.exit();
     }
   } else {
-    console.log("Please enter valid value from the table above");
+    console.log('Please enter valid value from the table above');
   }
   ac.abort();
-  rl.question("Your move: ", (answer) =>
+  rl.question('Your move: ', (answer) =>
     proceedInputOptions(answer, values, computerIndex, {
       checkWinner,
       help,
@@ -145,6 +146,10 @@ function getIndexOfComputerValue(values: Array<string>): number {
 
 function game() {
   const values = process.argv.slice(2);
+  if (!values.length || values.length % 2 === 0) {
+    console.log('Please enter odd number of turns as arguments');
+    process.exit();
+  }
   const rules = new Rules(values);
   const helpTable = new HelpTable(rules);
   const key = new KeyGenerator();
@@ -152,7 +157,7 @@ function game() {
   key.createKey();
   const computerIndex = getIndexOfComputerValue(values);
   hmac.createHmac(key.key, values[computerIndex]);
-  console.log("HMAC: ", hmac.hmac);
+  console.log('HMAC: ', hmac.hmac);
   rl.question(getOptions(values), { signal }, (answer) =>
     proceedInputOptions(answer, values, computerIndex, {
       checkWinner: rules.checkWinner,
